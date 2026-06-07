375918084c780e89bbff5ee8e4717492b51f6ab0
Mathias Scherer <mathischerer@gmail.com>
2026-06-06 22:06:19 -0300

    Refactor MainHeader and MainFooter styles for consistent border usage

--- client/src/lib/components/MainFooter.svelte
+++ client/src/lib/components/MainFooter.svelte
@@ -1,3 +1,3 @@
-<footer class="bg-teal-950 p-2 text-center text-white">
-  <p class="text-sm text-stone-400">&copy; 2026 LemkeBank. Todos os direitos reservados.</p>
+<footer class="bg-stone-950 p-2 text-center text-white border-t border-stone-500">
+  <p class="text-sm text-stone-500">&copy; 2026 LemkeBank. Todos os direitos reservados.</p>
 </footer>
--- client/src/lib/components/MainHeader.svelte
+++ client/src/lib/components/MainHeader.svelte
@@ -2,6 +2,6 @@
   import { resolve } from '$app/paths';
 </script>
 
-<header class="bg-teal-500 p-4 text-white">
+<header class="bg-teal-500 p-4 text-white border-b border-white">
   <a href={resolve('/wallet/overview')} class="font-[Stack_Sans_Notch] text-3xl">Lemke<b>Bank</b></a>
 </header>
