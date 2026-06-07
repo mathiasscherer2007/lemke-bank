d96d5d7090adab00a70c1139e48b7a6da39f3786
Mathias Scherer <mathischerer@gmail.com>
2026-06-07 12:07:06 -0300

    Add wallet layout for consistent structure across wallet pages

---
+++ client/src/routes/wallet/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import MainFooter from "$lib/components/MainFooter.svelte";
+    import MainHeader from "$lib/components/MainHeader.svelte";
+
+    let { children } = $props();
+</script>
+
+<div class="flex flex-col min-h-screen">
+	<MainHeader />
+	<main class="bg-stone-200 dark:bg-[rgb(27,27,26)] flex-1 px-30 flex flex-col">
+        {@render children()}
+    </main>
+    <MainFooter />
+</div>
