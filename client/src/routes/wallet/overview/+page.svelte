6964245fbc933537b7d9518a079abde29a56c6f7
Mathias Scherer <mathischerer@gmail.com>
2026-06-17 09:01:05 -0300

    Add title and other goodies to wallet overview

--- client/src/routes/wallet/overview/+page.svelte
+++ client/src/routes/wallet/overview/+page.svelte
@@ -13,11 +13,15 @@
   ] as const;
 </script>
 
+<svelte:head>
+  <title>Carteira | LemkeBank</title>
+</svelte:head>
+
 <div>
   <div class="flex w-full justify-between">
     <p class="text-xl">Saldo atual</p>
     <span class="flex items-center gap-2">
-      <p class="text-lg dark:text-gray-300">ID da carteira: 123456</p>
+      <span class="text-lg dark:text-gray-300">ID<span class="hidden ml-[6px] lg:inline">da carteira</span>: 123456</span>
       <button type="button">
         <img
           src={copyIcon}
@@ -30,11 +34,11 @@
   </div>
   <p class="lg:pl-2 text-2xl lg:text-3xl font-bold">BL$ 30</p>
 </div>
-<div class="mt-2 flex h-30 lg:h-40 overflow-x-scroll">
+<div class="mt-2 flex h-30 lg:h-40 overflow-x-auto overflow-y-hidden">
   {#each links as { title, icon, link } (link)}
     <a
       href={resolve(link)}
-      class="m-1 lg:m-3 flex aspect-square flex-col items-center justify-center rounded-xl border border-black p-2 text-center shadow-md transition hover:border-teal-500 dark:border-white dark:bg-[rgb(35,35,35)] dark:hover:border-teal-400"
+      class="m-1 lg:m-3 flex aspect-square flex-col items-center justify-center rounded-xl border border-black p-2 text-center shadow-md transition hover:border-teal-500 dark:border-white dark:bg-[#282828] dark:hover:border-teal-400"
     >
       <figure class="w-7/11 p-2 lg:p-3 white-filter">
         <img
