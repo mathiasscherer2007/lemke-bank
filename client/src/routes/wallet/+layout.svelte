b931ff59fb30d32320872289fb49998d24a497a4
Mathias Scherer <mathischerer@gmail.com>
2026-07-09 10:40:16 -0300

    Implement walletID transfer page mobile layout

--- client/src/routes/wallet/+layout.svelte
+++ client/src/routes/wallet/+layout.svelte
@@ -8,9 +8,11 @@
 <div class="flex min-h-screen flex-col">
   <MainHeader />
   <main class="flex flex-col flex-1 bg-stone-200/75 lg:px-20 dark:bg-[rgb(36,36,35)]">
-    <div class="flex flex-col flex-1 h-full px-5 lg:px-15 py-10 text-stone-900 bg-[#f1f1f1] dark:bg-[rgb(46,46,45)] dark:text-white">
+    <div class="flex flex-col flex-1 h-full px-3 lg:px-15 py-5 lg:py-10 text-stone-900 bg-[#f1f1f1] dark:bg-[rgb(46,46,45)] dark:text-white">
       {@render children()}
     </div>
   </main>
-  <MainFooter />
+  <span class="hidden lg:inline">
+    <MainFooter />
+  </span>
 </div>
--- client/src/routes/wallet/actions/pay/walletid/+page.svelte
+++ client/src/routes/wallet/actions/pay/walletid/+page.svelte
@@ -3,6 +3,8 @@
   import { resolve } from '$app/paths';
   import { fly } from 'svelte/transition';
 
+  import arrowIcon from "$lib/assets/icons/arrow-right.svg";
+
   let submitting = $state(false);
   let formStage = $state(0);
   let userData = $state({
@@ -35,10 +37,14 @@
   }
 </script>
 
+<a href={resolve('/wallet/actions/pay')} class="lg:hidden aspect-square h-10 flex items-center text-lg italic">
+  <img src={arrowIcon} alt="cancelar" class="white-filter rotate-180 h-full aspect-square">
+  cancelar
+</a>
 <form
   action="?/pay"
   method="post"
-  class="grid flex-1 place-items-center px-10 pb-15"
+  class="grid flex-1 place-items-center lg:px-10 lg:pb-15"
   autocomplete="off"
   use:enhance={({ cancel, formData }) => {
     if (!checkValues) {
@@ -58,26 +64,28 @@
       <div
         in:fly={{ delay: 500 }}
         out:fly
-        class="col-start-1 row-start-1 flex w-full flex-col overflow-x-hidden"
+        class="h-full lg:h-auto col-start-1 row-start-1 flex w-full flex-col justify-end overflow-x-hidden"
       >
-        <h1 class="mb-3 font-[Stack_Sans_Headline] text-3xl">
-          Digite ou cole o ID da carteira do recebedor
-        </h1>
-        <span class="p-3 pl-0">
-          <input
-            type="text"
-            name="reciever"
-            id="reciever"
-            placeholder="ID da carteira"
-            required
-            bind:value={userData.reciever}
-            class="border-b border-b-teal-500 p-1 text-2xl dark:border-b-teal-400"
-          />
-        </span>
-        <span class="mt-5 flex w-full justify-between gap-2">
+        <div class="my-auto">
+          <h1 class="mb-3 font-[Stack_Sans_Headline] text-3xl">
+            Digite ou cole o ID da carteira do recebedor
+          </h1>
+          <span class="m-3 ml-0 flex lg:block">
+            <input
+              type="text"
+              name="reciever"
+              id="reciever"
+              placeholder="ID da carteira"
+              required
+              bind:value={userData.reciever}
+              class="border-b border-b-teal-500 p-1 text-2xl dark:border-b-teal-400 flex-1 lg:flex-none"
+            />
+          </span>
+        </div>
+        <span class="mb-5 lg:mt-5 flex flex-col lg:flex-row w-full justify-between gap-2">
           <a
             href={resolve('/wallet/actions/pay')}
-            class="w-1/6 cursor-pointer rounded-xl border border-red-500 bg-transparent p-3 text-center font-bold text-red-500 transition hover:bg-[rgba(0,0,0,0.03)] dark:border-red-400 dark:text-red-400 dark:hover:bg-[rgba(255,255,255,0.05)]"
+            class="hidden lg:inline lg:w-1/6 text-xl lg:text-lg cursor-pointer rounded-xl border border-red-500 bg-transparent p-3 text-center font-bold text-red-500 transition hover:bg-[rgba(0,0,0,0.03)] dark:border-red-400 dark:text-red-400 dark:hover:bg-[rgba(255,255,255,0.05)]"
             >Cancelar</a
           >
           <button
@@ -86,7 +94,7 @@
             onclick={() => {
               moveToNextStep(userData.reciever);
             }}
-            class="w-1/4 cursor-pointer rounded-xl border border-teal-500 bg-teal-400 p-3 font-bold transition enabled:hover:bg-teal-500/80 disabled:cursor-default disabled:bg-transparent disabled:text-teal-500 dark:border-teal-400 dark:text-black dark:enabled:hover:bg-teal-500 dark:disabled:text-teal-400"
+            class="lg:w-1/4 text-xl lg:text-lg cursor-pointer rounded-xl border border-teal-500 bg-teal-400 p-3 font-bold transition enabled:hover:bg-teal-500/80 disabled:cursor-default disabled:bg-transparent disabled:text-teal-500 dark:border-teal-400 dark:text-black dark:enabled:hover:bg-teal-500 dark:disabled:text-teal-400"
             >Continuar</button
           >
         </span>
@@ -96,51 +104,53 @@
       <div
         in:fly={{ delay: 500 }}
         out:fly
-        class="col-start-1 row-start-1 flex w-full flex-col overflow-x-hidden"
+        class="h-full lg:h-auto col-start-1 row-start-1 flex w-full flex-col justify-end overflow-x-hidden"
       >
-        <h1 class="mb-3 font-[Stack_Sans_Headline] text-3xl">
-          Digite o valor que deseja transferir
-        </h1>
-        <span class="p-3 pl-0">
-          <label for="amount" class="text-2xl">BL$</label>
-          <input
-            type="number"
-            min="0"
-            name="amount"
-            id="amount"
-            required
-            bind:value={userData.amount}
-            class="w-[7ch] border-b border-b-teal-500 p-1 text-2xl dark:border-b-teal-400"
-          />
-        </span>
-        <span class="my-5 flex flex-col p-3 pt-0 pl-0">
-          <input
-            type="text"
-            name="description"
-            id="description"
-            placeholder="Descrição (opcional)"
-            bind:value={userData.description}
-            class="border-b border-b-teal-500 p-1 text-2xl dark:border-b-teal-400"
-          />
-        </span>
-        <span class="flex w-full justify-between gap-2">
+        <div class="my-auto">
+          <h1 class="mb-3 font-[Stack_Sans_Headline] text-3xl">
+            Digite o valor que será transferido
+          </h1>
+          <span class="p-3 pl-0">
+            <label for="amount" class="text-xl lg:text-2xl">BL$</label>
+            <input
+              type="number"
+              min="0"
+              name="amount"
+              id="amount"
+              required
+              bind:value={userData.amount}
+              class="w-[7ch] border-b border-b-teal-500 p-1 text-xl lg:text-2xl dark:border-b-teal-400"
+            />
+          </span>
+          <span class="my-5 flex p-3 pt-0 pl-0">
+            <input
+              type="text"
+              name="description"
+              id="description"
+              placeholder="Descrição (opcional)"
+              bind:value={userData.description}
+              class="border-b flex-1 border-b-teal-500 p-1 text-lg lg:text-2xl dark:border-b-teal-400"
+            />
+          </span>
+        </div>
+        <span class="flex flex-col lg:flex-row w-full justify-between gap-2 pb-5">
           <span class="flex flex-1 gap-2">
             <button
               type="button"
               onclick={returnStep}
-              class="w-1/4 cursor-pointer rounded-xl border border-teal-500 bg-teal-400 p-3 font-bold transition hover:bg-teal-500/80 dark:border-teal-400 dark:text-black dark:hover:bg-teal-500"
+              class="text-xl lg:text-lg lg:w-1/4 flex-1 lg:flex-none cursor-pointer rounded-xl border border-teal-500 bg-teal-400 p-3 font-bold transition hover:bg-teal-500/80 dark:border-teal-400 dark:text-black dark:hover:bg-teal-500"
               >Voltar</button
             >
             <a
               href={resolve('/wallet/actions/pay')}
-              class="w-1/6 cursor-pointer rounded-xl border border-red-500 bg-transparent p-3 text-center font-bold text-red-500 transition hover:bg-[rgba(0,0,0,0.03)] dark:border-red-400 dark:text-red-400 dark:hover:bg-[rgba(255,255,255,0.05)]"
+              class="hidden lg:inline w-1/6 cursor-pointer rounded-xl border border-red-500 bg-transparent p-3 text-center font-bold text-red-500 transition hover:bg-[rgba(0,0,0,0.03)] dark:border-red-400 dark:text-red-400 dark:hover:bg-[rgba(255,255,255,0.05)]"
               >Cancelar</a
             >
           </span>
           <button
             type="submit"
             disabled={userData.amount && userData.amount > 0 && userData.reciever ? false : true}
-            class="w-1/4 cursor-pointer rounded-xl border border-teal-500 bg-teal-400 p-3 font-bold transition enabled:hover:bg-teal-500/80 disabled:cursor-default disabled:bg-transparent disabled:text-teal-500 dark:border-teal-400 dark:text-black dark:enabled:hover:bg-teal-500 dark:disabled:text-teal-400"
+            class="text-xl lg:text-lg lg:w-1/4 flex-1 lg:flex-none cursor-pointer rounded-xl border border-teal-500 bg-teal-400 p-3 font-bold transition enabled:hover:bg-teal-500/80 disabled:cursor-default disabled:bg-transparent disabled:text-teal-500 dark:border-teal-400 dark:text-black dark:enabled:hover:bg-teal-500 dark:disabled:text-teal-400"
             >Enviar Pagamento</button
           >
         </span>
