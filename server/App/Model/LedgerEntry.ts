9cff6961780cedf4f48b1600366e650a1e1723d8
Nicolas <kochhann.nicolass.soldi@gmail.com>
2026-07-08 22:12:05 -0300

    Refactor LedgerEntry class to improve property definitions and constructor parameters

--- server/App/Model/LedgerEntry.ts
+++ server/App/Model/LedgerEntry.ts
@@ -1,19 +1,21 @@
 export class LedgerEntry
 {
-    private id: string;
+    private readonly id: string;
+    private transactionId: string | undefined;
+    private balanceAfter : number | undefined;
+    private counterpartyWalletId: string | undefined;
+    private readonly createdAt: Date;
 
     constructor(
-        private transactionId: string,
         private walletId: string,
         private type: 'debit' | 'credit',
         private amount: number,
         private balanceBefore: number,
-        private balanceAfter: number,
-        private counterpartyWalletId: string | null,
-        private createdAt: Date
     )
     {
         this.id = crypto.randomUUID();
+        this.balanceAfter = this.balanceBefore + (this.type === 'credit' ? this.amount : -this.amount);
+        this.createdAt = new Date();
     }
 
     public getWalletId(): string {
@@ -30,5 +32,9 @@ export class LedgerEntry
 
     public setTransactionId(transactionId: string): void {
         this.transactionId = transactionId;
+    }
+
+    public setCounterpartyWalletId(counterpartyWalletId: string): void {
+        this.counterpartyWalletId = counterpartyWalletId;
     }
 }
