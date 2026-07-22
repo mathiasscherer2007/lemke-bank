56d2436aabae62bebca9e8cd440fb8a18101b3d2
Mathias Scherer <mathischerer@gmail.com>
2026-07-22 19:17:58 -0300

    Create admin actions page

---
+++ client/src/routes/admin/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import MainFooter from '$lib/components/MainFooter.svelte';
+  import MainHeader from '$lib/components/MainHeader.svelte';
+
+  let { children } = $props();
+</script>
+
+<div class="flex min-h-screen flex-col">
+  <MainHeader />
+  <main class="flex flex-col flex-1 bg-stone-200/75 lg:px-20 dark:bg-[rgb(36,36,35)]">
+    <div class="flex flex-col flex-1 h-full px-3 lg:px-15 py-5 lg:py-10 text-stone-900 bg-[#f1f1f1] dark:bg-[rgb(46,46,45)] dark:text-white">
+      {@render children()}
+    </div>
+  </main>
+  <span class="hidden lg:inline">
+    <MainFooter />
+  </span>
+</div>
---
+++ client/src/routes/admin/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import payIcon from '$lib/assets/icons/pay.svg';
+  import statementIcon from '$lib/assets/icons/statement.svg';
+  import userSearch from '$lib/assets/icons/user-search.svg';
+  import createPayment from '$lib/assets/icons/create-payment.svg';
+
+  import SquareLink from '$lib/components/SquareLink.svelte';
+
+  const links = [
+    { title: 'Enviar BL$', icon: payIcon, link: '/admin/transfer' },
+    { title: 'Histórico de Envios', icon: statementIcon, link: '/admin/transferhistory' },
+    { title: 'Pesquisar Usuários', icon: userSearch, link: '/admin/usersearch' },
+    { title: 'Criar Pagamento', icon: createPayment, link: '/admin/createpayment' }
+  ] as const;
+</script>
+
+<h1 class="lg:pl-2 text-2xl lg:text-3xl font-bold">Ações de Administrador</h1>
+<div class="mt-2 flex h-30 overflow-x-auto overflow-y-hidden lg:h-40">
+  {#each links as { title, icon, link } (link)}
+    <SquareLink {title} {icon} {link} />
+  {/each}
+</div>
+<hr class="my-4 lg:my-6 border-stone-500" />
+<h2 class="lg:pl-2 text-xl lg:text-2xl font-bold">Visão Geral da Dashboard</h2>
