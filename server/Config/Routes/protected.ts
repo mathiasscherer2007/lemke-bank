494a50ec83dfbccfc0ed7834b637bf70d19eb512
Nicolas <kochhann.nicolass.soldi@gmail.com>
2026-07-26 16:22:16 -0300

    feat(tests): add smoke tests for AppContainer and AppServiceProvider; include environment variable handling in test scripts

--- server/Config/Routes/protected.ts
+++ server/Config/Routes/protected.ts
@@ -1,16 +1,11 @@
 import { FastifyPluginAsync } from "fastify";
-import { DrizzleTransactionRepository } from "../../App/Repository/Transaction/DrizzleTransactionRepository.js";
-import { TransactionProcessorService } from "../../App/Service/TransactionProcessorService.js";
-import { DrizzleWalletRepository } from "../../App/Repository/Wallet/DrizzleWalletRepository.js";
 import { TransactionController } from "../../App/Http/Controller/TransactionController.js";
 import { AuthMockMiddleware } from "../../App/Http/Middleware/AuthMockMiddleware.js";
-import { WalletManagementService } from "../../App/Service/WalletManagementService.js";
 import { WalletController } from "../../App/Http/Controller/WalletController.js";
-import { BrasilApiBusinessDayService } from "../../App/Service/WebService/BrasilApiBusinessDayService.js";
-import { env } from "../Environment/env.js";
 
 export const protectedRoutes: FastifyPluginAsync = async (app, options) => {
 
+    // TODO: This is just a example for now.
     const authMiddleware = app.container.get<AuthMockMiddleware>(AuthMockMiddleware);
     const walletController = app.container.get<WalletController>(WalletController);
     const transactionController = app.container.get<TransactionController>(TransactionController);
