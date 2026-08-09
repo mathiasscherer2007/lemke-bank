428f3e7bab47e0066ad1698a45baf3cf72d66448
Mathias Scherer <mathischerer@gmail.com>
2026-08-09 18:26:07 -0300

    Port over create payment pages to normal user routes

---
+++ client/src/routes/wallet/actions/createpayment/+page.server.ts
@@ -0,0 +1,13 @@
+import { redirect, type Actions } from "@sveltejs/kit";
+
+export const actions: Actions = {
+	createpayment: async ({ request }) => {
+		const data = await request.formData();
+
+		// Simulate delay
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+
+		const link = `lemkebank.test.com/${data.get("amount")}${data.get("description")}`;
+		throw redirect(303, `/wallet/actions/createpayment/viewlink?link=${link}`)
+	}
+};
