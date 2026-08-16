7d2dc2b9eb8c6b76081b2aed7865cd47d02cfea1
Nicolas <kochhann.nicolass.soldi@gmail.com>
2026-08-16 19:39:46 -0300

    refactor: streamline RedisTokenBlacklistingService by removing unnecessary connect method and improving client connection handling

--- server/App/Service/TokenBlacklistingService/RedisTokenBlacklistingService.ts
+++ server/App/Service/TokenBlacklistingService/RedisTokenBlacklistingService.ts
@@ -9,27 +9,27 @@ export class RedisTokenBlacklistingService
     private client: RedisClientType
 
     constructor(
-        tokenService: TokenService,
         host: string
     ){
         this.client = createClient({ url: host, keyPrefix: this.prefix })
             .on("error", (error) => { throw new CacheProviderException(error) });
-
-        await this.connect();
-    }
-
-    private async connect(): Promise<void>
-    {
-        await this.client.connect();
     }
 
     public async blacklist(decodedToken: JwtPayload): Promise<void>
     {
+        this.client.connect();
+        
         const currentTime = Math.floor(Date.now() / 1000);
         const { jti, exp } = decodedToken;
 
-        this.client.set(jti!, '0', { expiration: { type: 'EX', value: currentTime - exp! } });\
+        this.client.set(jti!, '0', { expiration: { type: 'EX', value: currentTime - exp! } });
     }
 
-    public async exists()
+    public async exists(decodedToken: JwtPayload): Promise<boolean>
+    {
+        this.client.connect();
+
+        const { jti } = decodedToken;
+        return await this.client.exists(this.prefix + jti) < 1;
+    }
 }
