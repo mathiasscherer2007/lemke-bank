07cf70a04a06be1dad42f41ce8b2e7cb61885297
Mathias Scherer <mathischerer@gmail.com>
2026-08-17 23:13:57 -0300

    Add logout button to header and functionality

---
+++ client/src/routes/account/logout/+page.server.ts
@@ -0,0 +1,24 @@
+import { env } from "$env/dynamic/private";
+import { fail, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+import { resolve } from "$app/paths";
+
+export const actions: Actions = {
+	logout: async ({ cookies, fetch }) => {
+		const response = await fetch(`${env.PRIVATE_API_URL}logout`, {
+			method: 'POST',
+			headers: {
+				'x-access-token': cookies.get('x-access-token') ?? '',
+				'x-refresh-token': cookies.get('x-refresh-token') ?? ''
+			}
+		});
+
+		if (!response.ok) {
+			return fail(response.status);
+		} else {
+			cookies.delete('x-access-token', { path: '/' });
+			cookies.delete('x-refresh-token', { path: '/' });
+			throw redirect(303, resolve('/account/login'));
+		}
+	},
+};
