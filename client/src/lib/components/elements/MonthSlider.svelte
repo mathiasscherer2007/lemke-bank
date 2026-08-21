91d7c9b94d52dfa259997675bb2e326d71314864
Mathias Scherer <mathischerer@gmail.com>
2026-08-21 09:50:24 -0300

    Refactor statement page to use API returned data

--- client/src/lib/components/elements/MonthSlider.svelte
+++ client/src/lib/components/elements/MonthSlider.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { Attachment } from "svelte/attachments";
 	import { enhance } from "$app/forms";
+	import { tick } from "svelte";
 
 	import arrowIcon from "$lib/assets/icons/arrow-right.svg";
 
@@ -14,7 +15,15 @@
 	let slider: HTMLElement;
 
 	const scrollToBottom: Attachment = (element) => {
-      element.scrollLeft = element.scrollWidth;
+      $effect(() => {
+			// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+			months;
+
+			tick().then(() => {
+				const buttons = element.querySelectorAll('button');
+				buttons[buttons.length - 1]?.click();
+			});
+		});
 	};
 
 	function scrollHorizontal(target: HTMLElement, direction: string) {
@@ -29,28 +38,28 @@
 <button type="button" onclick={() => { scrollHorizontal(slider, 'l') }} class="bg-neutral-200 dark:bg-neutral-700 rounded-l-xl p-2 cursor-pointer hover:bg-neutral-300 dark:hover:bg-neutral-600 transition hidden lg:inline">
 	<img src={arrowIcon} alt="<" class="white-filter w-5 rotate-180">
 </button>
-  <div {@attach scrollToBottom} class="flex flex-1 bg-neutral-200 dark:bg-neutral-700 overflow-x-auto p-2 scrollbar-none gap-2 rounded-xl lg:rounded-none" bind:this={slider}>
-      <div class="flex-[0_0_calc(50%-36px)]"></div>
-      
-      {#each months as { month, year, endpoint } ((month + year.toString()))}
-	  	<form action={buttonFormAction} method="POST" use:enhance class="h-20 flex-[0_0_80px] flex bg-red-50">
-			<input type="hidden" name="endpoint" value={endpoint}>
-			<button
-			type="submit" 
-			value={`${month}-${year}`} 
-			name="monthYear"
-			onclick={(event) => {event.currentTarget.scrollIntoView({behavior: "smooth", inline: "center", block: "nearest"})}} 
-			class="flex-1 flex flex-col aspect-square items-center justify-center text-center {`${month}-${year}` === selectedButton ? 'text-teal-600 dark:text-teal-400 bg-[#ced1cf] dark:bg-[#515251] font-bold transform-[scale(1.1)] rounded mx-1 scroll-mx-1' : 'bg-neutral-300 dark:bg-[#555555] cursor-pointer rounded-xl hover:bg-[#c8c5c3] dark:hover:bg-[#5d5d5d]'} transition-all transition-discrete"
-			disabled={`${month}-${year}` === selectedButton}
-			>
-				<span class="text-lg">{month}.</span>
-				<span class="text-sm">{year}</span>
-			</button>
-		</form>
-      {/each}
+<div {@attach scrollToBottom} class="flex flex-1 bg-neutral-200 dark:bg-neutral-700 overflow-x-auto p-2 scrollbar-none gap-2 rounded-xl lg:rounded-none" bind:this={slider}>
+	<div class="flex-[0_0_calc(50%-36px)]"></div>
+	
+	{#each months as { month, year, endpoint } ((month + year.toString()))}
+	<form action={buttonFormAction} method="POST" use:enhance={async () => {return async({ update }) => {await update({ invalidateAll: false });}}} class="h-20 flex-[0_0_80px] flex">
+		<input type="hidden" name="endpoint" value={endpoint}>
+		<button
+		type="submit" 
+		value={`${month}-${year}`} 
+		name="monthYear"
+		onclick={(event) => {event.currentTarget.scrollIntoView({behavior: "smooth", inline: "center", block: "nearest"})}} 
+		class="flex-1 flex flex-col aspect-square items-center justify-center text-center {`${month}-${year}` === selectedButton ? 'text-teal-600 dark:text-teal-400 bg-[#ced1cf] dark:bg-[#515251] font-bold transform-[scale(1.1)] rounded mx-1 scroll-mx-1' : 'bg-neutral-300 dark:bg-[#555555] cursor-pointer rounded-xl hover:bg-[#c8c5c3] dark:hover:bg-[#5d5d5d]'} transition-all transition-discrete"
+		disabled={`${month}-${year}` === selectedButton}
+		>
+			<span class="text-lg">{month}.</span>
+			<span class="text-sm">{year}</span>
+		</button>
+	</form>
+	{/each}
 
-      <div class="flex-[0_0_calc(50%-36px)]"></div>
-  </div>
-  <button type="button" onclick={() => { scrollHorizontal(slider, 'r') }} class="bg-neutral-200 dark:bg-neutral-700 rounded-r-xl p-2 cursor-pointer hover:bg-neutral-300 dark:hover:bg-neutral-600 transition  hidden lg:inline">
-	<img src={arrowIcon} alt="<" class="white-filter w-5">
-  </button>
+	<div class="flex-[0_0_calc(50%-36px)]"></div>
+</div>
+<button type="button" onclick={() => { scrollHorizontal(slider, 'r') }} class="bg-neutral-200 dark:bg-neutral-700 rounded-r-xl p-2 cursor-pointer hover:bg-neutral-300 dark:hover:bg-neutral-600 transition  hidden lg:inline">
+<img src={arrowIcon} alt="<" class="white-filter w-5">
+</button>
--- client/src/routes/wallet/actions/checkstatement/+page.svelte
+++ client/src/routes/wallet/actions/checkstatement/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-  import MonthSlider from '$lib/components/elements/MonthSlider.svelte';
+  import { numberToMonth } from '$lib/utils/date-utils';
   import type { PageProps } from './$types';
+  import { slide } from 'svelte/transition';
+  import { onMount } from 'svelte';
+
+  import MonthSlider from '$lib/components/elements/MonthSlider.svelte';
 
   import arrowinIcon from '$lib/assets/icons/arrow-in.svg';
   import arrowoutIcon from '$lib/assets/icons/arrow-out.svg';
-  import { onMount } from 'svelte';
-  import { numberToMonth } from '$lib/utils/date-utils';
 
   let { data, form }: PageProps = $props();
   let months: Array<{ month: string, year: number, endpoint: string | URL }> = $state([]);
@@ -16,12 +18,14 @@
       const month = numberToMonth(Number(link.searchParams.get('month') ?? 0), true);
       const year = Number(link.searchParams.get('year'));
 
-      months.push({
+      months.unshift({
         month: month,
         year: year,
         endpoint: link
       });
     });
+
+
   });
 </script>
 
@@ -35,7 +39,7 @@
 </div>
 
 {#snippet transfer(type: 'credit' | 'debit', title: string, amount: string, description?: string)}
-  <div class="flex gap-2 bg-[#dddddd50] px-2 py-3 dark:bg-neutral-700">
+  <div class="flex gap-2 bg-[#dddddd50] px-2 py-3 dark:bg-neutral-700 rounded">
     <div class="flex flex-col justify-evenly gap-2">
       <figure class="h-7 w-7 p-1 {type == 'credit' ? 'green-filter' : 'red-filter'}">
         <img src={type == 'credit' ? arrowinIcon : arrowoutIcon} alt={type} />
@@ -43,23 +47,27 @@
       <div class="mx-auto min-w-0.5 flex-1 rounded-xl bg-neutral-400"></div>
     </div>
     <div class="ml-1 flex flex-col gap-1">
-      <span class="m-0 text-xl">{title.toUpperCase()}</span>
-      <span class="text-md text-neutral-600 italic dark:text-neutral-300">{description}</span>
+      <span class="m-0 text-xl font-semibold">{title.toUpperCase()}</span>
+      <span class="text-md text-neutral-600 italic dark:text-neutral-300">{description ?? 'Nenhuma descrição fornecida.'}</span>
       <span class="text-lg font-bold">{type === 'debit' ? '-' : ''}BL$ {amount}</span>
     </div>
   </div>
 {/snippet}
 
-<div>
+<ol class="mt-3 flex flex-col gap-1">
   {#if form?.transactions.length > 0}
-    <!-- <ol in:slide class="mt-3 flex flex-col gap-1">
-      <li class="bg-[#bbbbbb95] p-2 text-lg font-bold dark:bg-neutral-800">25/03</li>
-      {@render transfer('credit', 'ana paula lemke', '10', 'Atividade de números binários')}
-      <li class="bg-[#bbbbbb95] p-2 text-lg font-bold dark:bg-neutral-800">24/03</li>
-      {@render transfer('debit', 'Túlio Lima Baségio', '6')}
-    </ol> -->
-    <p>{form?.transactions}</p>
+    {console.log("yep")}
+    {#each form?.transactions as { date, transactions }, index (index)}
+      <details open in:slide|global class="bg-[#bbbbbb95] text-lg font-bold dark:bg-neutral-800 rounded">
+        <summary class="p-2">{date}</summary>
+        <div class="flex flex-col gap-1 font-normal">
+          {#each transactions as { totalAmount, entries, description, transactionId } (transactionId)}
+            {@render transfer(entries[0].type, entries[0].relatedUser.username, totalAmount, description ?? '')}
+          {/each}
+        </div>
+      </details>
+    {/each}
   {:else}
     <p class="mt-5 text-xl opacity-80">Não houveram movimentações no período selecionado.</p>
   {/if}
-</div>
+</ol>
