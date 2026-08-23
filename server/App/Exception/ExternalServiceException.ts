12498515a6242a0f6788d579bc85923cfa67e4c9
Nicolas <kochhann.nicolass.soldi@gmail.com>
2026-08-23 20:13:08 -0300

    feat: enhance exception handling with error codes and add ErrorCodes documentation

--- server/App/Exception/ExternalServiceException.ts
+++ server/App/Exception/ExternalServiceException.ts
@@ -1,16 +1,18 @@
 abstract class ExternalServiceException extends Error 
 {
     public statusCode: number;
+    public readonly code: string;
 
-    constructor(message: string, statusCode: number) {
+    constructor(message: string, statusCode: number, code: string) {
         super(message);
         this.statusCode = statusCode;
+        this.code = code;
     }
 }
 
 export class ExternalBusinessDayServiceException extends ExternalServiceException
 {
     constructor(){
-        super("The external business day verification service doesn't respond", 502);
+        super("The external business day verification service doesn't respond", 502, 'BUSINESS_DAY_ERROR');
     }
 }
--- server/App/Exception/InfrastructureException.ts
+++ server/App/Exception/InfrastructureException.ts
@@ -1,10 +1,12 @@
 abstract class InfrastructureException extends Error 
 {
     public statusCode: number;
+    public readonly code: string;
 
-    constructor(message: string, statusCode: number) {
+    constructor(message: string, statusCode: number, code: string) {
         super(message);
         this.statusCode = statusCode;
+        this.code = code;
     }
 }
 
@@ -13,7 +15,7 @@ export class ServiceProviderTokenNotFound extends InfrastructureException
     private readonly missingToken: any
 
     constructor(missingToken: any){
-        super('AppContainer could not resolve all service providers', 500);
+        super('AppContainer could not resolve all service providers', 500, 'SERVICE_PROVIDER_MISSING');
         this.missingToken = missingToken;
         this.message = `AppContainer could not resolve service provider for ${missingToken}`;
     }
@@ -24,7 +26,7 @@ export class CacheProviderException extends InfrastructureException
     private readonly error;
 
     constructor(error: Error){
-        super('Cache provider error', 500);
+        super('Cache provider error', 500, 'CACHE_PROVIDER_ERROR');
         this.error = error;
     }
 }
